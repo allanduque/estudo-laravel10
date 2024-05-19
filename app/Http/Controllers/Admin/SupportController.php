5dbad1a9fdbd5c2adc3868a45f8f190e6660a22c
Allan Duque dos Santos <allan.santos@modalgr.com.br>
2024-05-19 19:20:40 -0300

    create migration table support

--- app/Http/Controllers/Admin/SupportController.php
+++ app/Http/Controllers/Admin/SupportController.php
@@ -3,12 +3,17 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\Support;
 use Illuminate\Http\Request;
 
 class SupportController extends Controller
 {
-    public function index()
+    public function index(Support $support)
     {
+        $supports = $support->all();
+
+        dd($supports);
+
         return view('admin/supports/index');
     }
 }
